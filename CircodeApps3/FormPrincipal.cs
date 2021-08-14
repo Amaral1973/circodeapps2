9120df80c0a6143c331cc225fc1768cb0153435e
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2021-08-14 10:49:44 -0300

    Circo de Apps
    Aplicativo desenvolvido em C# usando Windows Forms, pela turma TDS05.

---
+++ CircodeApps3/FormPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircodeApps3
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void picboxConversordeMoeda_Click(object sender, EventArgs e)
+        {
+            FormConversorMoeda conversor = new FormConversorMoeda();
+            conversor.Show();
+        }
+    }
+}
